ef639a67a7d0e860a8b066c75471e12272a27ff3
sjseo <muiruat@naver.com>
2021-12-21 02:12:43 +0900

    필드 클래스 만들기

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 'use strict';
+import PopUp from './popup.js';
 
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
@@ -12,10 +13,6 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
-const popUp = document.querySelector('.pop-up');
-const popUpRefresh = document.querySelector('.pop-up__refresh');
-const popUpMessage = document.querySelector('.pop-up__message');
-
 const carrotSound = new Audio('/sound/carrot_pull.mp3');
 const alertSound = new Audio('/sound/alert.wav');
 const bgSound = new Audio('/sound/bg.mp3');
@@ -26,6 +23,11 @@ let started = false;
 let score = 0;
 let timer = undefined;
 
+const gameFinishBanner = new PopUp();
+gameFinishBanner.setClickListener(() => {
+  startGame();
+});
+
 field.addEventListener('click', onFieldClick);
 
 gameBtn.addEventListener('click', () => {
@@ -34,11 +36,6 @@ gameBtn.addEventListener('click', () => {
   } else {
     startGame();
   }
-});
-
-popUpRefresh.addEventListener('click', () => {
-  startGame();
-  hidePopUp();
 });
 
 function startGame() {
@@ -54,7 +51,7 @@ function stopGame() {
   started = false;
   stopGameTimer();
   hideGameButton();
-  showPopUpWithText('REPLAY❓');
+  gameFinishBanner.showWithText('REPLAY❓');
   playSound(alertSound);
   stopSound(bgSound);
 }
@@ -69,7 +66,7 @@ function finishGame(win) {
   }
   stopGameTimer();
   stopSound(bgSound);
-  showPopUpWithText(win ? 'YOU WON 🎉' : 'YOU LOST 💩');
+  gameFinishBanner.showWithText(win ? 'YOU WON 🎉' : 'YOU LOST 💩');
 }
 
 function showStopButton() {
@@ -113,15 +110,6 @@ function updateTimerText(time) {
 
 function stopGameTimer() {
   clearInterval(timer);
-}
-
-function showPopUpWithText(text) {
-  popUpMessage.innerText = text;
-  popUp.classList.remove('pop-up--hide');
-}
-
-function hidePopUp() {
-  popUp.classList.add('pop-up--hide');
 }
 
 function initGame() {
